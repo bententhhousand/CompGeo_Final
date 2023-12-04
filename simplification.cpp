84c77f5f65299d5a76791e3c99454b132fc2bb7e
bententhhousand <bententhhousand@gmail.com>
2023-12-04 15:09:05 -0500

    Added kd tree simplification and first-pass octree simplification

--- simplification.cpp
+++ simplification.cpp
@@ -49,7 +49,7 @@ typedef CGAL::Bbox_3                                            Bbox;
 typedef CGAL::Surface_mesh<Point_3>                             Mesh;
 typedef Mesh::Vertex_index                                      Vertex_descriptor;
 typedef CGAL::Orthtrees::Preorder_traversal                     Preorder_traversal;
-
+typedef CGAL::Orthtrees::Leaves_traversal                     	Leaves_traversal;
 
 void print_kd_bounds(const Kd_tree_rectangle& rect) {
     // Get the bounds for each dimension
@@ -70,10 +70,9 @@ double euclidean_distance(Point_3 a, Point_3 b) {
     return sqrt(pow(a.x() - b.x(), 2) + pow(a.y() - b.y(), 2) + pow(a.z() - b.z(), 2));
 }
 
-
 double nearest_point_distance(Point_set &set1, Point_set &set2) {
     
-    double total_distance = 0;
+    double final_distance = euclidean_distance(set1.point(*set1.begin()), set2.point(*set2.begin()));
     for (Point_set::const_iterator it = set1.begin(); it != set1.end(); it++) {
         double min_distance = euclidean_distance(set1.point(*it), set2.point(*set2.begin()));
         for (Point_set::const_iterator jt = set2.begin(); jt != set2.end(); jt++) {
@@ -82,9 +81,22 @@ double nearest_point_distance(Point_set &set1, Point_set &set2) {
                 min_distance = distance;
             }
         }
-        total_distance += min_distance;
+        if (min_distance < final_distance){
+			final_distance = min_distance;
+		}
     }
-    return total_distance / set1.size();
+    return final_distance;
+}
+
+double average_point_distance(Point_set &set1, Point_set &set2) {
+    
+    double final_distance = 0;
+    for (Point_set::const_iterator it = set1.begin(); it != set1.end(); it++) {
+        for (Point_set::const_iterator jt = set2.begin(); jt != set2.end(); jt++) {
+            final_distance += euclidean_distance(set1.point(*it), set2.point(*jt));
+        }
+    }
+    return final_distance / (set1.size() * set2.size());
 }
 
 void printBbox(Bbox& inputBox){
@@ -148,16 +160,30 @@ void add_cube_to_mesh(Mesh &mesh, double xmin, double xmax, double ymin, double 
     mesh.add_edge(v2, v6);
     mesh.add_edge(v3, v7);
     mesh.add_edge(v4, v8);
+}
+Point_3 bbox_center(Bbox bbox){
+	double x_center = (bbox.xmin() + bbox.xmax())/2.0;
+	double y_center = (bbox.ymin() + bbox.ymax())/2.0;
+	double z_center = (bbox.zmin() + bbox.zmax())/2.0;
 
+	return Point_3(x_center, y_center, z_center);
 }
 
+Point_3 bbox_center_kd(const Kd_tree_rectangle& bbox){
+    double x_center = (bbox.min_coord(0) + bbox.max_coord(0))/2.0;
+	double y_center = (bbox.min_coord(1) + bbox.max_coord(1))/2.0;
+	double z_center = (bbox.min_coord(2) + bbox.max_coord(2))/2.0;
 
-void construct_kd_mesh(const Node* node, const Kd_tree_rectangle& bbox, Mesh &mesh) {
+	return Point_3(x_center, y_center, z_center);
+}
+
+void construct_kd_mesh(const Node* node, const Kd_tree_rectangle& bbox, Mesh &mesh, Point_set &averagedPoints) {
     if (!node) return;
 
     if (node -> is_leaf()) {
         // Process the points in the leaf node
         const Leaf_node* leaf_node = static_cast<const Leaf_node*>(node);
+        averagedPoints.insert(bbox_center_kd(bbox));
     } else {
         const Internal_node* internal_node = static_cast<const Internal_node*>(node);
         // print_kd_bounds(bbox);
@@ -170,13 +196,66 @@ void construct_kd_mesh(const Node* node, const Kd_tree_rectangle& bbox, Mesh &me
         // Now you can use lower_bbox and upper_bbox for further processing
 
         // Recursively traverse the child nodes
-        construct_kd_mesh(internal_node->lower(), lower_bbox, mesh);
-        construct_kd_mesh(internal_node->upper(), upper_bbox, mesh);
+        construct_kd_mesh(internal_node->lower(), lower_bbox, mesh, averagedPoints);
+        construct_kd_mesh(internal_node->upper(), upper_bbox, mesh, averagedPoints);
     }
 }
 
 
-int main(int argc, char* argv[]){ // cmake -G"Visual Studio 16" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:/Users/matth/Documents/CompGeo/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+Point_set octree_middles(Octree octree){
+
+	Point_set set;
+
+	double counts = 0;
+	int number = 0;
+
+	for (Octree::Node node : octree.traverse<Leaves_traversal>()) {
+		if (!node.empty()){
+			counts += node.size();
+        	set.insert(bbox_center(octree.bbox(node)));
+			number ++;
+		}
+    }
+
+	std::cout << "Average = " << counts/number << std::endl;
+
+	return set;
+}
+
+
+// Point_set octree_averages(Octree octree){
+// 	Point_set set;
+
+// 	int countSum = 0;
+// 	int countCount = 0;
+
+// 	for (Octree::Node node : octree.traverse<Leaves_traversal>()) {
+// 		if (!node.empty()){
+// 			double xAvg = 0;
+// 			double zAvg = 0;
+// 			double yAvg = 0;
+// 			int count = 0;
+// 			auto currentPoint = node.begin();
+// 			while (currentPoint != node.end()){
+// 				Point_3 point = octree.point(*currentPoint);
+// 				xAvg += point.x();
+// 				yAvg += point.y();
+// 				zAvg += point.z();
+// 				count ++;
+// 			}
+//         	set.insert(Point_3(xAvg/count, yAvg/count, zAvg/count));
+// 			countSum += count;
+// 			countCount ++;
+// 		}
+//     }
+
+// 	std::cout << "Average of " << countSum / countCount << " points per filled box" << endl;
+
+// 	return set;
+// }
+
+int main(int argc, char* argv[]){ 
+	// cmake -G"Visual Studio 16" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:/Users/matth/Documents/CompGeo/vcpkg/scripts/buildsystems/vcpkg.cmake ..
     // cmake --build .
 
     Point_set points;
@@ -208,7 +287,7 @@ int main(int argc, char* argv[]){ // cmake -G"Visual Studio 16" -A x64 -DCMAKE_T
 	}
 
 	Kd_Tree ktree(points.points().begin(), points.points().end(), bucketSize);
-	CGAL::draw(inputMesh);
+	//CGAL::draw(inputMesh);
 
     Octree octree(points, points.point_map());
     octree.refine(100000, bucketSize);
@@ -225,7 +304,7 @@ int main(int argc, char* argv[]){ // cmake -G"Visual Studio 16" -A x64 -DCMAKE_T
     std::copy (points.points().begin(), points.points().end(), std::back_inserter (vertices));
     Mesh output_mesh;
     CGAL::Polygon_mesh_processing::polygon_soup_to_polygon_mesh (vertices, facets, output_mesh);
-    CGAL::draw(output_mesh);
+    //CGAL::draw(output_mesh);
     
     for (Octree::Node node : octree.traverse<Preorder_traversal>()) {
         Bbox nodeBox = octree.bbox(node);
@@ -233,24 +312,58 @@ int main(int argc, char* argv[]){ // cmake -G"Visual Studio 16" -A x64 -DCMAKE_T
         add_cube_to_mesh(inputMesh, nodeBox.xmin(), nodeBox.xmax(), nodeBox.ymin(), nodeBox.ymax(), nodeBox.zmin(), nodeBox.zmax());
         //std::cout << node << "\n" << std::endl;
     }
-    CGAL::draw(inputMesh);
+    //CGAL::draw(inputMesh);
 
     // Test point distance
     Point_set cloud1;
-    cloud1.insert(Point_3(1, 2, 3));
-    cloud1.insert(Point_3(4, 5, 6));
-    cloud1.insert(Point_3(7, 8, 9));
-
     Point_set cloud2;
-    cloud2.insert(Point_3(1, 2, 3));
-    cloud2.insert(Point_3(4, 5, 6));
-    cloud2.insert(Point_3(7, 8, 10));
-    cloud2.insert(Point_3(1, 2.5, 3));
-    cloud2.insert(Point_3(7, 1, 10));
 
-    double distance = nearest_point_distance(cloud1, cloud2);
-    std::cout << "Point cloud Distance: " << distance << std::endl;
+    cloud1.insert(Point_3(1, 0, 0));
+    cloud2.insert(Point_3(1, 0, 5));
+
+	std::cout << "Point cloud distances (Should both be 5): " << nearest_point_distance(cloud1, cloud2) << ", " <<nearest_point_distance(cloud2, cloud1) << std::endl;
+
+    cloud2.insert(Point_3(1, 0, 10));
+
+	std::cout << "Point cloud distances (Should still both be 5): " << nearest_point_distance(cloud1, cloud2) << ", " <<nearest_point_distance(cloud2, cloud1) << std::endl;
+
+    cloud2.insert(Point_3(1, 0, 2));
+
+	std::cout << "Point cloud distances (Should both be 2): " << nearest_point_distance(cloud1, cloud2) << ", " <<nearest_point_distance(cloud2, cloud1) << std::endl;
+
+    cloud2.insert(Point_3(1, 0, 0));
+
+	std::cout << "Point cloud distances (Should both be 0): " << nearest_point_distance(cloud1, cloud2) << ", " <<nearest_point_distance(cloud2, cloud1) << std::endl;
+
+    // cloud1.insert(Point_3(4, 5, 6));
+    // cloud1.insert(Point_3(7, 8, 9));
+
+    // cloud2.insert(Point_3(4, 5, 6));
+    // cloud2.insert(Point_3(7, 8, 10));
+    // cloud2.insert(Point_3(1, 2.5, 3));
+    // cloud2.insert(Point_3(7, 1, 10));
+
+    // double distance = nearest_point_distance(cloud1, cloud2);
+    // std::cout << "Point cloud Distance: " << distance << std::endl;
+	// std::cout << "Other distance: "<< nearest_point_distance(cloud2, cloud1) << std::endl;
     // Iterate over kd tree
-    construct_kd_mesh(ktree.root(), ktree.bounding_box(), inputKdMesh);
-    CGAL::draw(inputKdMesh);
+    Point_set kdMiddles;
+    construct_kd_mesh(ktree.root(), ktree.bounding_box(), inputKdMesh, kdMiddles);
+    //CGAL::draw(inputKdMesh);
+
+	CGAL::draw(points);
+	Point_set middles = octree_middles(octree);
+
+    
+    std::cout << "KD-Tree Simplification" << std::endl;
+    
+	CGAL::draw(kdMiddles);
+	std::cout << nearest_point_distance(points, kdMiddles) << ", " << average_point_distance(points, kdMiddles) << std::endl;
+
+    std::cout << "Octree Simplification" << std::endl;
+    
+	CGAL::draw(middles);
+	std::cout << nearest_point_distance(points, middles) << ", " << average_point_distance(points, middles) << std::endl;
+
+
 }
